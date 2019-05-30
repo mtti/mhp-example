453ac5c855da6d802e8eedd49033c84e4f6d1748
mtti <mtti@users.noreply.github.com>
2019-05-30 13:13:02 +0300

    Import middleware with new names

--- mhp.routes.js
+++ mhp.routes.js
@@ -1,14 +1,14 @@
-const { middleware } = require('@mtti/mhp');
+const { generatePostPages, generatePostIndexes, generateAtomFeed } = require('@mtti/mhp');
 
 module.exports = function(router) {
   router.setGlobals({
     siteTitle: 'MHP Test Site',
   });
 
-  router.get('/posts/:category/:slug', middleware.posts());
-  router.get('/', middleware.indexes({}));
+  router.get('/posts/:category/:slug', generatePostPages());
+  router.get('/', generatePostIndexes());
 
-  router.get('/atom.xml', middleware.atom({
+  router.get('/atom.xml', generateAtomFeed({
     uuid: '120024cf-8721-4492-b4de-eb6847c92abb',
     title: 'MHP Test Site Main Atom Feed',
   }));
