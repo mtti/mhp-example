d79d6fd27ae43d0fa705379f7a6ea52ce60da69b
mtti <mtti@users.noreply.github.com>
2019-09-28 21:42:28 +0300

    Use pipe-based build script format

--- build-content.js
+++ build-content.js
@@ -1,26 +1,30 @@
 const {
   feed,
   build,
+  branch,
   indexes,
   posts,
   page,
+  vars,
 } = require('@mtti/mhp');
 
-const vars = {
-  siteTitle: 'MHP Test Site',
-  baseUrl: 'http://127.0.0.1',
-  lang: 'en',
-};
+build(
+  __dirname,
 
-build(__dirname, vars, async ({ emit }) => {
-  await emit('/posts/:category/:slug', posts());
-  await emit('/', indexes());
+  vars({
+    siteTitle: 'MHP Test Site',
+    baseUrl: 'http://127.0.0.1',
+    lang: 'en',
+  }),
 
-  await emit('/feed', feed({
+  branch('/posts/:category/:slug', posts()),
+  branch('/', indexes()),
+
+  branch('/feed', feed({
     uuid: '120024cf-8721-4492-b4de-eb6847c92abb',
     title: 'MHP Test Site Main Atom Feed',
     maxPosts: 50,
-  }));
+  })),
 
-  await emit('/about.html', page('about.md'));
-});
+  branch('/about.html', page('about.md')),
+);
