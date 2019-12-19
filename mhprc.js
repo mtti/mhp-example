5a1639cd06d69fec3b035fe2eda47e05bd8e4ea8
mtti <mtti@users.noreply.github.com>
2019-12-19 09:41:19 +0200

    Use new MHP version

--- mhprc.js
+++ mhprc.js
@@ -1,6 +1,5 @@
 const {
-  feed,
-  build,
+  rss,
   branch,
   indexes,
   posts,
@@ -8,9 +7,7 @@ const {
   vars,
 } = require('@mtti/mhp');
 
-build(
-  __dirname,
-
+module.exports = [
   vars({
     siteTitle: 'MHP Test Site',
     baseUrl: 'http://127.0.0.1',
@@ -20,11 +17,11 @@ build(
   branch('/posts/:category/:slug', posts()),
   branch('/', indexes()),
 
-  branch('/feed', feed({
+  branch('/feed', rss({
     uuid: '120024cf-8721-4492-b4de-eb6847c92abb',
     title: 'MHP Test Site Main Atom Feed',
     maxPosts: 50,
   })),
 
   branch('/about.html', page('about.md')),
-);
+];
